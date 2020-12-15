328a3d0b83ca6af9ecd634df880cc9bcd5d22e8a
qqq33 <67838883+qqq33@users.noreply.github.com>
2020-12-16 00:46:46 +0300

    qqq333

---
+++ цццц/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using ConsoleApp333;
+
+namespace погода
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string key = "3ada81cc47ee18b768e559b614add3da";
+            string city = "";
+
+            Console.WriteLine("Введите город: \n");
+            city = Console.ReadLine();
+            Console.WriteLine();
+
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + key;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(result);
+
+                Console.WriteLine("Temperature in " + weatherResponse.Name + ": " + weatherResponse.Main.Temp + "°C \n");
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ цццц/TemperatureInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp333
+{
+    class TemperatureInfo
+    {
+        public float Temp { get; set; }
+    }
+}
